3e8053f5550daac430db7ecf50cfb6f44932fd41
Tugran Yayla <tugrandev@gmail.com>
2023-03-22 13:07:28 +0300

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Tuğran on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
